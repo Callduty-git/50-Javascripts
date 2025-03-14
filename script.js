d8314c4bb0247c80742d22c7af3f7f4497583763
Jupazago <juanpablozabla@gmail.com>
2025-03-14 16:01:48 -0500

    ejercicio 2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //Ejercicio 1:	Print "Hello, World!" to the console.
-console.log("Hola, Mundo")
+/*console.log("Hola, Mundo")*/
 //Ejercicio 2:	Declare and assign variables of different data types.
+let nombre = ("Juan Pablo Zabala Gomez");
+let edad = ("18");
+let fechanacimiento = ("22 de mayo del 2006");
+let lugarnacimiento = ("Bogotá")
+console.log ("hola yo soy " + nombre + " tengo " + edad + " nací el " + fechanacimiento + " y soy de " + lugarnacimiento);
+ 
