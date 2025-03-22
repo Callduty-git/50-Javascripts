0ab3fd94fbc84d90d587e32f6c5a3711869b0a23
Jupazago <juanpablozabla@gmail.com>
2025-03-22 13:35:51 -0500

    ejercicio 10

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
 //Ejercicio 1:	Print "Hello, World!" to the console.
 /*console.log("Hola, Mundo")*/
+
+
 //Ejercicio 2:	Declare and assign variables of different data types.
-let nombre = ("Juan Pablo Zabala Gomez");
+/*let nombre = ("Juan Pablo Zabala Gomez");
 let edad = ("18");
 let fechanacimiento = ("22 de mayo del 2006");
 let lugarnacimiento = ("Bogotá")
-console.log ("hola yo soy " + nombre + " tengo " + edad + " nací el " + fechanacimiento + " y soy de " + lugarnacimiento);
- 
+console.log ("hola yo soy " + nombre + " tengo " + edad + " nací el " + fechanacimiento + " y soy de " + lugarnacimiento);*/
+
+
+//Ejercicio 3:   Write a function that returns the sum of two numbers.
+/*let numero1 = parseInt(prompt("Digita un numero"));
+let numero2 = parseInt(prompt("Digita otro numero"));
+let resultado = numero1 + numero2;
+alert ("La suma de los dos numeros es: " + resultado);*/
+
+
+//Ejercicio 4:	Create a function that checks if a number is even or odd.
+/*let numero = parseInt(prompt("Digite el numero"));
+let resultado = numero % 2
+if (resultado === 0){
+   alert ("El numero dijitado es par")
+}else{
+    alert ("El numero dijitado es impar")
+}*/
+
+
+//Ejercicio 5:	Write a loop that prints numbers from 1 to 10.
+/*let limite = parseInt(prompt("Digite el número límite"));
+
+for (let i = 1; i <= limite; i++) {
+    console.log(i);
+}*/
+
+
+//Ejercicio 6:	Create an array and iterate through it using a loop.
+/*let bucle = parseInt(prompt("Digite el número"));
+
+for (let i = 1; i <= bucle; i++) {
+    console.log(i);
+
+
+//Ejercicio 7:	Write a function that reverses a string.
+/*let texto = prompt("Escribe el texto")
+let textoreverso = function(s) {
+    return s.split("").reduce((rev, char)=> char + rev, '');
+};
+alert (textoreverso(texto));*/
+
+//Ejercicio 8:	Create an object with properties and access them.
+/*let otto ={
+    es: "Un gato",
+    come: "3 veses al día"
+};
+    otto.es = "Un elegfante";
+    console.log (otto);*/
+
+
+//Ejercicio 9:	Convert a string to uppercase.
+/*let texto = prompt("Escribe el texto")
+let textomayus = texto.toUpperCase();
+alert (textomayus);*/
+
+
+//Ejercicio 10:	Create a function that checks if a word is a palindrome.
